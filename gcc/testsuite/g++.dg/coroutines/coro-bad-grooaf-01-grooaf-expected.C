bac201a0895c363e88daf798b1db4b6887ab81ed
François Dumont <fdumont@gcc.gnu.org>
2023-09-21 06:47:52 +0200

    testsuite: Add optional libstdc++ version namespace in expected diagnostic
    When libstdc++ is build with --enable-symvers=gnu-versioned-namespace diagnostics are
    showing this namespace, currently __9.
    
    gcc/testsuite/ChangeLog:
    
    	* testsuite/g++.dg/coroutines/coro-bad-alloc-00-bad-op-new.C: Add optional
    	'__8' version namespace in expected diagnostic.
    	* testsuite/g++.dg/coroutines/coro-bad-alloc-01-bad-op-del.C: Likewise.
    	* testsuite/g++.dg/coroutines/coro-bad-alloc-02-no-op-new-nt.C: Likewise.
    	* testsuite/g++.dg/coroutines/coro-bad-grooaf-01-grooaf-expected.C: Likewise.
    	* testsuite/g++.dg/coroutines/pr97438.C: Likewise.
    	* testsuite/g++.dg/coroutines/ramp-return-b.C: Likewise.

--- gcc/testsuite/g++.dg/coroutines/coro-bad-grooaf-01-grooaf-expected.C
+++ gcc/testsuite/g++.dg/coroutines/coro-bad-grooaf-01-grooaf-expected.C
@@ -6,7 +6,7 @@
 int used_grooaf = 0;
 
 struct coro1
-f () noexcept // { dg-warning {'operator new' is marked 'throw\(\)' or 'noexcept' but no usable 'get_return_object_on_allocation_failure' is provided by 'std::__n4861::__coroutine_traits_impl<coro1, void>::promise_type' \{aka 'coro1::promise_type'\}} }
+f () noexcept // { dg-warning {'operator new' is marked 'throw\(\)' or 'noexcept' but no usable 'get_return_object_on_allocation_failure' is provided by 'std::(__9::)?__n4861::__coroutine_traits_impl<coro1, void>::promise_type' \{aka 'coro1::promise_type'\}} }
 {
   PRINT ("coro1: about to return");
   co_return;
